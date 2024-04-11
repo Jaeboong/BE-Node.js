dc8e4651bd02e6400617eea2af488f337510aba2
Jaeboong <cbkjh0225@gmail.com>
2024-04-12 01:19:39 +0900

    create blog

--- doit-node/myContacts/controllers/loginController.js
+++ doit-node/myContacts/controllers/loginController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
 
 // GET login page
 // GET /
@@ -11,15 +14,21 @@ const getLogin = (req, res) => {
 
 // Login user
 // POST /
-const loginUser = asyncHandler((req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     const {username, password}  = req.body;
+    const user = await User.findOne({username});
+    if(!user){
+        return res.json({message: '일치하는 사용자가 없습니다.'});
+    }
 
-    if (username === "admin" && password === "1234"){
-        res.send("Login success");
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.json({ message: "비밀번호가 일치하지 않습니다."});
     }
-    else{
-        res. send("Login Failed");
-    }
+
+    const token = jwt.sign({id:user._id}, jwtSecret);
+    res.cookie("token", token, {httpOnly: true});
+    res.redirect("/contacts");
 })
 
 const getRegister = (req, res) => {
