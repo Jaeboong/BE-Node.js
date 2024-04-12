390799b59e7f849494d5dbd60bd3752171df910e
Jaeboong <cbkjh0225@gmail.com>
2024-04-12 17:14:03 +0900

    4.12 update

--- doit-node/myBlog/app.js
+++ doit-node/myBlog/app.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db");
+const Post = require("./models/Post");
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+connectDb();
+
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.static("public"));
+
 app.use("/", require("./routes/main"));
+app.use("/", require("./routes/admin"));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
--- doit-node/myBlog/routes/main.js
+++ doit-node/myBlog/routes/main.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asyncHandler = require("express-async-handler");
 
-router.get(["/", "/home"], (req, res) => {
+router.get(["/", "/home"],
+ asyncHandler (async(req, res) => {
     const locals = {
         title: "Home"
     }
-    res.render("index", {locals, layout: mainLayout});
-})
+    const data = await Post.find();
+    res.render("index", {locals, data, layout: mainLayout});
+}))
 
 router.get("/about", (req, res) => {
     const locals = {
@@ -17,3 +21,38 @@ router.get("/about", (req, res) => {
  })
 
 module.exports = router;
+
+// GET /post/:id
+// 게시물 상세 보기
+
+router.get(
+    "/post/:id",
+    asyncHandler(async (req, res) =>{
+        const data = await Post.findOne({_id: req.params.id});
+        res.render("post", {data, layout: mainLayout});
+    })
+);
+
+/*
+Post.insertMany([
+    {
+        title: "Title1",
+        body: "내용 1 - 123123123" 
+    },
+
+    {
+        title: "Title2",
+        body: "내용 2 - 123123123" 
+    },
+
+    {
+        title: "Title3",
+        body: "내용 3 - 123123123" 
+    },
+
+    {
+        title: "Title4",
+        body: "내용 4 - 123123123" 
+    }
+])
+*/
