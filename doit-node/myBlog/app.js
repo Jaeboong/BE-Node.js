f4598dc270fddd7b63de60441c407fb75c12aa30
Jaeboong <cbkjh0225@gmail.com>
2024-04-13 01:37:52 +0900

    4.13 update

--- doit-node/myBlog/app.js
+++ doit-node/myBlog/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const connectDb = require("./config/db");
 const Post = require("./models/Post");
+const cookieParser = require("cookie-parser");
+const methodOverride =require("method-override");
 
 
 const app = express();
@@ -13,6 +15,12 @@ connectDb();
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
+app.use(methodOverride("_method"));
 
 app.use(express.static("public"));
 
